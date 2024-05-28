257974361c9c995623977c85caab71d166406a41
2024076026 <billy4205@naver.com>
2024-05-28 09:04:06 +0900

    submission of files

---
+++ week03-1/problem1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+	int n ; 
+	unsigned long c = 0x48616E79616E6700UL;
+	printf("Enter the position :");
+	unsigned long div= 0x1UL
+	scanf("%d",&n);
+
+	for (i =7 ; i>=pos ; i--){
+	div *=0x0100UL;
+	}
+
+	*/unsigned char byte = ( c>>(n*8))&0xFF;
+	int intValue =(int)byte;
+	if (intValue >= 'a' &&intValue <='z'){
+		intValue = intValue-'a'+'A';
+	}/*
+	//printf("%c\n",intValue);
+	int letter = c/div
+	printf(" pos : %d div : %x\n",pos , div);
+	printf("Q : %lx\n,",(byt)(c/div));
+	printf("Q : %lx\n",(char)(c/div));
+	return 0;
+}
